69c2ad48d513904f72d1ef0a569ec7a574cd661e
Brianvu7836 <112970264+Brianvu7836@users.noreply.github.com>
2022-10-20 09:19:05 -0700

    Add files via upload

---
+++ Projectset1-BrianVu.py
@@ -0,0 +1,42 @@
+
+
+'''def isMultiple(a, b):
+    if a%b == 0:
+        print(b, "is a multiple of", a)
+    else:
+        print(b, "is not a multiple of", a)
+for f in range(2):
+    choose = int(input("type 1(Name) or 2(Number): "))
+
+    if choose == 1:
+        name = input("Please enter your name: ")
+        age = int(input("Please enter your age: "))
+        height = float(input("please enter your height: "))
+    
+        print("Oh,"+name+", is it? It is nice to meet you!")
+        print("Have a good day "+name)
+        print("You are "+str(age)+" years old")
+        print("You are "+str(height)+"cm tall")
+
+    elif choose == 2:
+        a = int(input("type the larger number: "))
+        b = int(input("type the lower number: "))
+        isMultiple(a, b)'''
+
+def Palindrome(string):
+    large = len(string)
+    g = 0
+    h = large - 1
+    while g < h:
+        if string[g]  != string[h]:
+            print ("False")
+            break
+        g = g + 1
+        h = h - 1
+    print ("True")
+def l():
+            Palindrome("racecar")
+l()
+
+print("-----------------------")
+    
